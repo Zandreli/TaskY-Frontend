06be2edc9cb171d6f8d911b13730a1d537b12a7e
Zandrelius <zandreliusakain1@gmail.com>
2025-07-30 12:30:17 +0300

    F Changes

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -25,10 +25,10 @@ export const taskService = {
   getActiveTasks: () => api.get<{ message: string; tasks: Task[] }>("/tasks"),
 
   getCompletedTasks: () =>
-    api.get<{ message: string; tasks: Task[] }>(`/tasks/completed/`),
+    api.get<{ message: string; tasks: Task[] }>(`/tasks/completed`),
 
   getDeletedTasks: () =>
-    api.get<{ message: string; tasks: Task[] }>(`/tasks/trash/`),
+    api.get<{ message: string; tasks: Task[] }>(`/tasks/trash`),
 
   getTaskById: (id: string) =>
     api.get<{ message: string; task: Task }>(`/tasks/${id}`),
